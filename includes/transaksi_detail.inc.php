8d6a6bc4d419a8e55eb8a71e962d43d746b2e4d6
marceldeveloper9 <marceldeveloper9@gmail.com>
2022-08-14 21:04:14 +0700

    update transaksi

--- includes/transaksi_detail.inc.php
+++ includes/transaksi_detail.inc.php
@@ -1,34 +1,30 @@
 <?php
 
-class Transaksi {
+class TransaksiDetail {
     private $conn;
-    private $table_transaksi = 'transaksi';
+    private $table_transaksi_detail = 'transaksi_detail';
 
+    public $id_transaksi_detail;
     public $id_transaksi;
-    public $id_user;
-    public $tgl_transaksi;
-    public $metode_pembayaran;
-    public $total_harga;
-    public $status;
-    public $id_diskon;
-    public $no_meja;
+    public $id_produk;
+    public $harga;
+    public $jumlah;
 
     public function __construct($db) {
 		$this->conn = $db;
 	}
 
-
     function insert() {
-        $query = "INSERT INTO {$this->table_transaksi} 
-		(id_user, tgl_transaksi, total_harga, status) 
-		VALUES(:id_user, :tgl_transaksi, :total_harga, :status)";
+        $query = "INSERT INTO {$this->table_transaksi_detail} 
+		(id_transaksi, id_produk, harga, jumlah) 
+		VALUES(:id_transaksi, :id_produk, :harga, :jumlah)";
 
         $stmt = $this->conn->prepare($query);
         // produk
-        $stmt->bindParam(':id_user', $this->id_user);
-		$stmt->bindParam(':tgl_transaksi', $this->tgl_transaksi);
-        $stmt->bindParam(':total_harga', $this->total_harga);
-        $stmt->bindParam(':status', $this->status);
+        $stmt->bindParam(':id_transaksi', $this->id_transaksi);
+		$stmt->bindParam(':id_produk', $this->id_produk);
+        $stmt->bindParam(':harga', $this->harga);
+        $stmt->bindParam(':jumlah', $this->jumlah);
 
 		if ($stmt->execute()) {
             // var_dump($stmt);
@@ -52,7 +48,7 @@ class Transaksi {
 	}
 
 	function genCode($latest, $key, $chars = 0) {
-        $new = intval(substr($latest, strlen($key)));
+        $new = intval(substr($latest, strlen($key))) + 1;
         $numb = str_pad($new, $chars, "0", STR_PAD_LEFT);
         return $key . $numb;
 	}
@@ -72,20 +68,16 @@ class Transaksi {
 		$stmt->execute();
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if($row == ""){
-            return true;
-        }else{
-            $data['id_transaksi'] = $row['id_transaksi'];
-            $data['id_user'] = $row['id_user'];
-            $data['tgl_transaksi'] = $row['tgl_transaksi'];
-            $data['metode_pembayaran'] = $row['metode_pembayaran'];
-            $data['total_harga'] = $row['total_harga'];
-            $data['status'] = $row['status'];
-            $data['id_diskon'] = $row['id_diskon'];
-            $data['no_meja'] = $row['no_meja'];
-            return $data;
-        }
+		$data['id_transaksi'] = $row['id_transaksi'];
+        $data['id_user'] = $row['id_user'];
+		$data['tgl_transaksi'] = $row['tgl_transaksi'];
+		$data['metode_pembayaran'] = $row['metode_pembayaran'];
+        $data['total_harga'] = $row['total_harga'];
+        $data['status'] = $row['status'];
+        $data['id_diskon'] = $row['id_diskon'];
+        $data['no_meja'] = $row['no_meja'];
 
+        return $data;
 	}
 
 	function update() {
@@ -108,24 +100,6 @@ class Transaksi {
         $stmt->bindParam(':foto', $this->foto);
         $stmt->bindParam(':keterangan', $this->keterangan);
         $stmt->bindParam(':id_produk', $this->id_produk);
-
-		if ($stmt->execute()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	function updateHarga() {
-		$query = "UPDATE {$this->table_transaksi}
-			SET
-                total_harga = :total_harga,
-			WHERE
-				id_transaksi = :id_transaksi";
-        $stmt = $this->conn->prepare($query);
-
-		$stmt->bindParam(':id_transaksi', $this->id_transaksi_update);
-		$stmt->bindParam(':total_harga', $this->total_harga);
 
 		if ($stmt->execute()) {
 			return true;
---
+++ transaksi_post.php
@@ -0,0 +1,48 @@
+<?php
+    include("config.php");
+    include_once('includes/produk.inc.php');
+    include_once('includes/transaksi.inc.php');
+    include_once('includes/transaksi_detail.inc.php');
+    session_start();
+    $id_user = $_SESSION['id_user'];
+    $config = new Config(); 
+    $db = $config->getConnection();
+    $Produk = new Produk($db);
+    $Transaksi = new Transaksi($db);
+    $TransaksiDetail = new TransaksiDetail($db);
+
+    $id_produk = $_POST["id_produk"];
+    $jumlah_barang = $_POST["jumlah"];
+
+    if($_POST && $jumlah_barang > 0){
+        $Transaksi->id_user =  $_SESSION['id_user'];
+        $Transaksi->tgl_transaksi =  date('Y-m-d');
+        $Transaksi->total_harga = 0;
+        $Transaksi->status =  "belum bayar";
+        $Produk->id_produk = $id_produk;
+        $Produk->readOne();
+        $cek_transaksi = $Transaksi->readOne();
+        // print('<pre>');print_r($cek_transaksi);exit();
+        if($cek_transaksi != 1){
+            $TransaksiDetail->id_transaksi =  $cek_transaksi['id_transaksi'];
+            $TransaksiDetail->id_produk =  $id_produk;
+            $TransaksiDetail->harga =  $Produk->harga;
+            $TransaksiDetail->jumlah =  $jumlah_barang;
+            $TransaksiDetail->insert();
+            header("Location: index.php");
+        }else{
+            $Transaksi->insert();
+            $last_id = $Transaksi->getNewID();
+            $TransaksiDetail->id_transaksi =  $last_id;
+            $TransaksiDetail->id_produk =  $id_produk;
+            $TransaksiDetail->harga =  $Produk->harga;
+            $TransaksiDetail->jumlah =  $jumlah_barang;
+            $TransaksiDetail->insert();
+            $Transaksi->id_transaksi_update = $last_id;
+            $Transaksi->total_harga = $Produk->harga;
+            $Transaksi->updateHarga();
+            // header("Location: index.php");
+        }
+    }else{
+        // return false;
+    }
